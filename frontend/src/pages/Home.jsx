1e53a29e51885ab7a735a5907658c1f43403a606
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-29 09:51:16 -0500

    added router

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+import userService from "../services/user-service";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  componentDidMount() {
+    userService.getPublicContent().then(
+      response => {
+        this.setState({
+          content: response.data
+        });
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response && error.response.data && error.response.data.message) ||
+            error.message ||
+            error.toString()
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h3>Home</h3>
+        </header>
+        <p>{this.state.content}</p>
+      </div>
+    );
+  }
+}
