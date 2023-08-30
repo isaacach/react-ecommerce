88bcd7aa2ea8ec93ff6bbcc9d77cdf0c4f0c066d
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-30 10:18:30 -0500

    added security config and user services

---
+++ backend/src/main/java/com/ecommerce/backend/models/User.java
@@ -0,0 +1,41 @@
+package com.ecommerce.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Size(max = 20)
+  private String username;
+
+  @NotBlank
+  @Size(max = 50)
+  @Email
+  private String email;
+
+  @NotBlank
+  @Size(max = 100)
+  private String password;
+  
+}
---
+++ backend/src/main/java/com/ecommerce/backend/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.ecommerce.backend.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.backend.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String username);
+
+}
